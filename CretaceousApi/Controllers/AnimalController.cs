7ec4e28d6214dff57eba13f9c58182ead29f6a85
onurkaymak <onurkaymak@outlook.com>
2023-10-21 11:23:46 -0700

    Add Put route in AnimalController.

--- CretaceousApi/Controllers/AnimalController.cs
+++ CretaceousApi/Controllers/AnimalController.cs
@@ -44,5 +44,41 @@ namespace CretaceousApi.Controllers
 
       return animal;
     }
+
+    // PUT: api/Animals/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Animals.Update(animal);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
   }
 }
